76dbcbdec7e8bfe8489dcab13884e0e7088071d8
Julia2063 <sky206380@gmail.com.com>
2025-11-06 18:10:28 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
-// write code here
+/* eslint-disable no-console */
+
+const { rename } = require('fs/promises');
+const { statSync, existsSync } = require('fs');
+const path = require('path');
+
+async function app() {
+  const args = process.argv.slice(2);
+  const [source, destination] = args;
+
+  if (!source || !destination) {
+    console.error(`Two arguments was needed`);
+
+    return;
+  }
+
+  try {
+    const isDir =
+      existsSync(destination) && statSync(destination).isDirectory();
+
+    const finDest = isDir
+      ? path.join(destination, path.basename(source))
+      : destination;
+
+    await rename(source, finDest);
+    console.log(`${source} was moved to ${destination}`);
+  } catch (e) {
+    console.error(`The file could not be moved. Error: ${e}`);
+  }
+}
+
+app();
+
+module.exports = { app };
